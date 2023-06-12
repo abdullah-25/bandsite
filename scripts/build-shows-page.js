5b73dbe72ba4adcebac283e8e879a9c1ffbe5543
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-06-12 16:13:07 -0400

    add axios get to shows page, display dynamic content under shows section, change date formatting

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,122 +1,103 @@
-let shows = [
-  {
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Tue Sept 21 2021",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Oct 15 2021",
-    venue: "View Lounge",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-    location: "San Francisco, CA",
-  },
-];
+const url = "https://project-1-api.herokuapp.com/";
+const api_key = "a64cef94-0295-4c01-8470-4e776116845e";
 
-let showsContainer = document.querySelector(".shows__container");
+let shows = [];
 
-for (let show of shows) {
-  showDisplay(show);
-}
+function displayShows() {
+  axios.get(`${url}showdates?api_key=${api_key}`).then((response) => {
+    shows = response.data;
+    console.log(shows);
+    for (let i = 0; i < shows.length; i++) {
+      //date div
+      let showsContainerDate = document.createElement("div");
+      showsContainerDate.classList.add("shows__container--date");
 
-function showDisplay(show) {
-  //date div
-  let showsContainerDate = document.createElement("div");
-  showsContainerDate.classList.add("shows__container--date");
+      let showsContainerDateTitle = document.createElement("div");
+      showsContainerDateTitle.classList.add("shows__container--date--title");
+      showsContainerDateTitle.innerText = "DATE";
 
-  let showsContainerDateTitle = document.createElement("div");
-  showsContainerDateTitle.classList.add("shows__container--date--title");
-  showsContainerDateTitle.innerText = "DATE";
+      let showsContainerDateActualValue = document.createElement("div");
+      showsContainerDateActualValue.classList.add(
+        "shows__container--date--actualvalue"
+      );
 
-  let showsContainerDateActualValue = document.createElement("div");
-  showsContainerDateActualValue.classList.add(
-    "shows__container--date--actualvalue"
-  );
-  showsContainerDateActualValue.innerText = show.date;
+      let d = new Date(shows[i].date);
 
-  showsContainerDate.appendChild(showsContainerDateTitle);
-  showsContainerDate.appendChild(showsContainerDateActualValue);
-  //showsContainer.appendChild(showsContainerDate);
+      showsContainerDateActualValue.innerText = d.toDateString();
 
-  //venue div
-  let venueContainer = document.createElement("div");
-  venueContainer.classList.add("shows__container--venue");
+      showsContainerDate.appendChild(showsContainerDateTitle);
+      showsContainerDate.appendChild(showsContainerDateActualValue);
+      //showsContainer.appendChild(showsContainerDate);
 
-  let venueContainerTitle = document.createElement("div");
-  venueContainerTitle.classList.add("shows__container--venue--title");
-  venueContainerTitle.innerText = "VENUE";
+      //venue div
+      let venueContainer = document.createElement("div");
+      venueContainer.classList.add("shows__container--venue");
 
-  let venueContainerActualValue = document.createElement("div");
-  venueContainerActualValue.classList.add(
-    "shows__container--venue--actualvalue"
-  );
-  venueContainerActualValue.innerText = show.venue;
+      let venueContainerTitle = document.createElement("div");
+      venueContainerTitle.classList.add("shows__container--venue--title");
+      venueContainerTitle.innerText = "VENUE";
 
-  venueContainer.appendChild(venueContainerTitle);
-  venueContainer.appendChild(venueContainerActualValue);
-  //showsContainer.appendChild(venueContainer);
+      let venueContainerActualValue = document.createElement("div");
+      venueContainerActualValue.classList.add(
+        "shows__container--venue--actualvalue"
+      );
+      venueContainerActualValue.innerText = shows[i].place;
 
-  //location div
-  let locationContainerDate = document.createElement("div");
-  locationContainerDate.classList.add("shows__container--location");
+      venueContainer.appendChild(venueContainerTitle);
+      venueContainer.appendChild(venueContainerActualValue);
+      //showsContainer.appendChild(venueContainer);
 
-  let locationContainerDateTitle = document.createElement("div");
-  locationContainerDateTitle.classList.add("shows__container--location--title");
-  locationContainerDateTitle.innerText = "LOCATION";
+      //location div
+      let locationContainerDate = document.createElement("div");
+      locationContainerDate.classList.add("shows__container--location");
 
-  let locationContainerDateActualValue = document.createElement("div");
-  locationContainerDateActualValue.classList.add(
-    "shows__container--location--actualvalue"
-  );
-  locationContainerDateActualValue.innerText = show.location;
+      let locationContainerDateTitle = document.createElement("div");
+      locationContainerDateTitle.classList.add(
+        "shows__container--location--title"
+      );
+      locationContainerDateTitle.innerText = "LOCATION";
 
-  locationContainerDate.appendChild(locationContainerDateTitle);
-  locationContainerDate.appendChild(locationContainerDateActualValue);
-  //showsContainer.appendChild(locationContainerDate);
+      let locationContainerDateActualValue = document.createElement("div");
+      locationContainerDateActualValue.classList.add(
+        "shows__container--location--actualvalue"
+      );
+      locationContainerDateActualValue.innerText = shows[i].location;
 
-  //button div
-  let button = document.createElement("button");
-  let buttonContainer = document.createElement("div");
-  buttonContainer.className = "shows__container--btn";
-  button.classList.add("shows__container--btn-buy-tickets");
-  button.innerHTML = "BUY TICKETS";
-  buttonContainer.appendChild(button);
+      locationContainerDate.appendChild(locationContainerDateTitle);
+      locationContainerDate.appendChild(locationContainerDateActualValue);
+      //showsContainer.appendChild(locationContainerDate);
 
-  let showsOuterContainer = document.createElement("div");
-  showsOuterContainer.className = "shows__container__showsOuterContainer";
-  showsOuterContainer.appendChild(showsContainerDate);
-  showsOuterContainer.appendChild(venueContainer);
-  showsOuterContainer.appendChild(locationContainerDate);
-  showsOuterContainer.appendChild(buttonContainer);
+      //button div
+      let button = document.createElement("button");
+      let buttonContainer = document.createElement("div");
+      buttonContainer.className = "shows__container--btn";
+      button.classList.add("shows__container--btn-buy-tickets");
+      button.innerHTML = "BUY TICKETS";
+      buttonContainer.appendChild(button);
 
-  showsContainer.appendChild(showsOuterContainer);
+      let showsOuterContainer = document.createElement("div");
+      showsOuterContainer.className = "shows__container__showsOuterContainer";
+      showsOuterContainer.appendChild(showsContainerDate);
+      showsOuterContainer.appendChild(venueContainer);
+      showsOuterContainer.appendChild(locationContainerDate);
+      showsOuterContainer.appendChild(buttonContainer);
 
-  //line divider
+      showsContainer.appendChild(showsOuterContainer);
 
-  let lineDivider = document.createElement("hr");
-  lineDivider.classList.add("shows__container--line-divider");
+      //line divider
 
-  showsContainer.appendChild(lineDivider);
+      let lineDivider = document.createElement("hr");
+      lineDivider.classList.add("shows__container--line-divider");
 
-  showsOuterContainer.addEventListener("click", () => {
-    showsOuterContainer.classList.toggle("selected");
+      showsContainer.appendChild(lineDivider);
+
+      showsOuterContainer.addEventListener("click", () => {
+        showsOuterContainer.classList.toggle("selected");
+      });
+    }
   });
 }
+
+displayShows();
+
+let showsContainer = document.querySelector(".shows__container");
