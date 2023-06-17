0828a5a068b8a6e75aca21bff2b0826c78e76f39
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-06-17 18:10:50 -0400

    add changes based on sprint-2 feedback

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -6,7 +6,10 @@ let shows = [];
 function displayShows() {
   axios.get(`${url}showdates?api_key=${api_key}`).then((response) => {
     shows = response.data;
-    for (let i = 0; i < shows.length; i++) {
+    const firstShow = shows[0];
+    specialFirstDiv(firstShow);
+    console.log(shows[0]);
+    for (let i = 1; i < shows.length; i++) {
       //date div
       let showsContainerDate = document.createElement("div");
       showsContainerDate.classList.add("shows__container--date");
@@ -89,14 +92,125 @@ function displayShows() {
       lineDivider.classList.add("shows__container--line-divider");
 
       showsContainer.appendChild(lineDivider);
-
-      showsOuterContainer.addEventListener("click", () => {
-        showsOuterContainer.classList.toggle("selected");
-      });
     }
   });
 }
 
+const showsContainer = document.querySelector(".shows__container");
+
 displayShows();
 
-let showsContainer = document.querySelector(".shows__container");
+// Add event listener after displayShows() function
+showsContainer.addEventListener("click", (event) => {
+  const clickedElement = event.target;
+  const outerContainer = clickedElement.closest(
+    ".shows__container__showsOuterContainer"
+  );
+  if (outerContainer) {
+    outerContainer.classList.toggle(
+      "shows__container__showsOuterContainer--selected"
+    );
+    const otherContainers = showsContainer.querySelectorAll(
+      ".shows__container__showsOuterContainer"
+    );
+    otherContainers.forEach((container) => {
+      if (container !== outerContainer) {
+        container.classList.remove(
+          "shows__container__showsOuterContainer--selected"
+        );
+      }
+    });
+  }
+});
+
+const showsContain = document.querySelectorAll(
+  ".shows__container__showsOuterContainer"
+);
+function hideName() {
+  for (let i = 1; i < showsContain.length; i++) {}
+}
+
+function specialFirstDiv(firstShow) {
+  //date div
+  let showsContainerDate = document.createElement("div");
+  showsContainerDate.classList.add("shows__container--date");
+
+  let showsContainerDateTitle = document.createElement("div");
+  showsContainerDateTitle.classList.add("shows__container--date--firsttitle");
+  showsContainerDateTitle.innerText = "DATE";
+
+  let showsContainerDateActualValue = document.createElement("div");
+  showsContainerDateActualValue.classList.add(
+    "shows__container--date--firstactualvalue"
+  );
+
+  let d = new Date(firstShow.date);
+
+  showsContainerDateActualValue.innerText = d.toDateString();
+
+  showsContainerDate.appendChild(showsContainerDateTitle);
+  showsContainerDate.appendChild(showsContainerDateActualValue);
+  //showsContainer.appendChild(showsContainerDate);
+
+  //venue div
+  let venueContainer = document.createElement("div");
+  venueContainer.classList.add("shows__container--venue");
+
+  let venueContainerTitle = document.createElement("div");
+  venueContainerTitle.classList.add("shows__container--venue--firsttitle");
+  venueContainerTitle.innerText = "VENUE";
+
+  let venueContainerActualValue = document.createElement("div");
+  venueContainerActualValue.classList.add(
+    "shows__container--venue--firstactualvalue"
+  );
+  venueContainerActualValue.innerText = firstShow.place;
+
+  venueContainer.appendChild(venueContainerTitle);
+  venueContainer.appendChild(venueContainerActualValue);
+  //showsContainer.appendChild(venueContainer);
+
+  //location div
+  let locationContainerDate = document.createElement("div");
+  locationContainerDate.classList.add("shows__container--location");
+
+  let locationContainerDateTitle = document.createElement("div");
+  locationContainerDateTitle.classList.add(
+    "shows__container--location--firsttitle"
+  );
+  locationContainerDateTitle.innerText = "LOCATION";
+
+  let locationContainerDateActualValue = document.createElement("div");
+  locationContainerDateActualValue.classList.add(
+    "shows__container--location--firstactualvalue"
+  );
+  locationContainerDateActualValue.innerText = firstShow.location;
+
+  locationContainerDate.appendChild(locationContainerDateTitle);
+  locationContainerDate.appendChild(locationContainerDateActualValue);
+  //showsContainer.appendChild(locationContainerDate);
+
+  //button div
+  let button = document.createElement("button");
+  let buttonContainer = document.createElement("div");
+  buttonContainer.className = "shows__container--btn";
+  button.classList.add("shows__container--btn-buy-tickets");
+  button.innerHTML = "BUY TICKETS";
+  buttonContainer.appendChild(button);
+
+  let showsOuterContainer = document.createElement("div");
+  showsOuterContainer.className = "shows__container__showsOuterContainer";
+  showsOuterContainer.appendChild(showsContainerDate);
+  showsOuterContainer.appendChild(venueContainer);
+  showsOuterContainer.appendChild(locationContainerDate);
+  showsOuterContainer.appendChild(buttonContainer);
+
+  showsContainer.appendChild(showsOuterContainer);
+
+  //line divider
+
+  let lineDivider = document.createElement("hr");
+  lineDivider.classList.add("shows__container--line-divider");
+
+  showsContainer.appendChild(lineDivider);
+}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,20 +1,18 @@
-const url_index = "https://project-1-api.herokuapp.com/";
-const api_key_index = "a64cef94-0295-4c01-8470-4e776116845e";
+const urlIndex = "https://project-1-api.herokuapp.com/";
+const apiKeyIndex = "a64cef94-0295-4c01-8470-4e776116845e";
 
-let parentClassUserComments = document.querySelector(
+const parentClassUserComments = document.querySelector(
   ".user-comments-container"
 );
-
 const formSubmission = document.querySelector(
   ".conversation__comment-contaier__comment-outer-container--form"
 );
-
 const formErrorName = document.getElementById("name");
-const formErroComment = document.getElementById("comment");
+const formErrorComment = document.getElementById("comment");
 
-let conversationArray = "";
+let conversationArray = [];
 
-axios.get(`${url_index}comments?api_key=${api_key_index}`).then((response) => {
+axios.get(`${urlIndex}comments?api_key=${apiKeyIndex}`).then((response) => {
   conversationArray = response.data;
   conversationArray.sort(
     (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
@@ -25,90 +23,81 @@ axios.get(`${url_index}comments?api_key=${api_key_index}`).then((response) => {
 
 formSubmission.addEventListener("submit", (event) => {
   event.preventDefault();
-  let dateTime = new Date();
-  let date = dateTime.toLocaleDateString();
+  const name = event.target.name.value.trim();
+  const comment = event.target.comment.value.trim();
 
-  if (
-    event.target.name.value.trim() === "" ||
-    event.target.comment.value.trim() === ""
-  ) {
+  if (name === "" || comment === "") {
     formErrorName.classList.add(
       "conversation__comment-contaier__comment-outer-container--form--error"
     );
-    formErroComment.classList.add(
+    formErrorComment.classList.add(
       "conversation__comment-contaier__comment-outer-container--form--error"
     );
-    // if (!event.target.name.value || !event.target.comment.value) {
-    //   return res.status(400).json({ error: 'Both name and comment are required' });
-    // }
     return;
   } else {
     formErrorName.classList.remove(
       "conversation__comment-contaier__comment-outer-container--form--error"
     );
-    formErroComment.classList.remove(
+    formErrorComment.classList.remove(
       "conversation__comment-contaier__comment-outer-container--form--error"
     );
   }
 
-  addComment(event.target.name.value, date, event.target.comment.value);
+  const dateTime = new Date();
+  const date = dateTime.toLocaleDateString();
+
+  addComment(name, date, comment);
 
   axios
-    .post(`${url_index}comments?api_key=${api_key_index}`, {
-      name: event.target.name.value,
-      comment: event.target.comment.value,
-    })
-    .then(function (response) {
+    .post(`${urlIndex}comments?api_key=${apiKeyIndex}`, { name, comment })
+    .then((response) => {
       conversationArray.unshift(response.data);
       displayComments();
+    })
+    .catch((error) => {
+      console.error(error);
     });
-  if (!event.target.name.value || !event.target.comment.value) {
-    return res
-      .status(400)
-      .json({ error: "Both name and comment are required" });
-  }
 
-  // Clear the input fields
   event.target.name.value = "";
   event.target.comment.value = "";
 });
 
 function addComment(name, date, comment) {
-  let newImage = document.createElement("img");
+  const newImage = document.createElement("img");
   newImage.className = "user-comments-container__image-container--avatar";
 
-  let imageDiv = document.createElement("div");
+  const imageDiv = document.createElement("div");
   imageDiv.className = "user-comments-container__image-container";
   imageDiv.appendChild(newImage);
 
-  let newOuterDiv = document.createElement("div");
+  const newOuterDiv = document.createElement("div");
   newOuterDiv.className = "user-comments-container__outerdiv";
   newOuterDiv.appendChild(imageDiv);
 
-  let nameAndEmailDiv = document.createElement("div"); // New div for name and email
-  nameAndEmailDiv.className = "user-comments-container__name-email-div"; // Replace with desired class name
+  const nameAndEmailDiv = document.createElement("div");
+  nameAndEmailDiv.className = "user-comments-container__name-email-div";
 
-  let newName = document.createElement("div");
+  const newName = document.createElement("div");
   newName.className = "user-comments-container__outerdiv--name";
   newName.innerHTML = name;
   nameAndEmailDiv.appendChild(newName);
 
-  let newEmail = document.createElement("div");
+  const newEmail = document.createElement("div");
   newEmail.className = "user-comments-container__outerdiv--email";
   newEmail.innerHTML = date;
   nameAndEmailDiv.appendChild(newEmail);
 
-  newOuterDiv.appendChild(nameAndEmailDiv); // Append the new div containing name and email
+  newOuterDiv.appendChild(nameAndEmailDiv);
 
-  let newComment = document.createElement("div");
+  const newComment = document.createElement("div");
   newComment.className = "user-comments-container__outerdiv--comment";
   newComment.innerHTML = comment;
   newOuterDiv.appendChild(newComment);
 
-  let likebtnContainer = document.createElement("div");
+  const likebtnContainer = document.createElement("div");
   likebtnContainer.className =
     "user-comments-container__outerdiv--likebtnContainer";
-  let likebtn = document.createElement("button");
+  const likebtn = document.createElement("button");
   likebtn.className = "user-comments-container__outerdiv--likebtn";
 
   likebtnContainer.appendChild(likebtn);
@@ -116,10 +105,10 @@ function addComment(name, date, comment) {
   likebtn.innerHTML = `Like ${comment.likes}`;
   newOuterDiv.appendChild(likebtnContainer);
 
-  let deletebtnContainer = document.createElement("div");
-  likebtnContainer.className =
+  const deletebtnContainer = document.createElement("div");
+  deletebtnContainer.className =
     "user-comments-container__outerdiv--deletebtnContainer";
-  let deletebtn = document.createElement("button");
+  const deletebtn = document.createElement("button");
   deletebtn.className = "user-comments-container__outerdiv--deletebtn";
 
   deletebtnContainer.appendChild(deletebtn);
@@ -127,77 +116,81 @@ function addComment(name, date, comment) {
   deletebtn.innerHTML = `Delete`;
   newOuterDiv.appendChild(deletebtnContainer);
 
-  if (likebtn) {
-    likebtn.addEventListener("click", function () {
-      likebtn.innerHTML = `Like ${comment.likes + 1}`;
-      comment.likes += 1;
-    });
-  } else {
-    console.log("Element with the specified selector was not found.");
-  }
-
-  if (deletebtn) {
-    likebtn.addEventListener("click", function () {
-      likebtn.innerHTML = `Like ${comment.likes + 1}`;
-      comment.likes += 1;
-    });
-  } else {
-    console.log("Element with the specified selector was not found.");
-  }
-
-  let newHr = document.createElement("hr");
+  const newHr = document.createElement("hr");
   newOuterDiv.appendChild(newHr);
 
   parentClassUserComments.appendChild(newOuterDiv);
-  parentClassUserComments.appendChild(newHr); // Append the <hr> element at the bottom
-}
+  parentClassUserComments.appendChild(newHr);
 
-// Function to display the comments
+  likebtn.addEventListener("click", () => {
+    likebtn.innerHTML = `Like ${comment.likes + 1}`;
+    comment.likes += 1;
+  });
+
+  deletebtn.addEventListener("click", () => {
+    newOuterDiv.remove();
+    const hr = document.querySelector(
+      ".conversation__comment-contaier--line-break"
+    );
+
+    axios
+      .delete(`${urlIndex}comments/${comment.id}/?api_key=${apiKeyIndex}`)
+      .then((response) => {
+        console.log(comment.id);
+        conversationArray = conversationArray.filter(
+          (com) => com.id !== comment.id
+        );
+        displayComments();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+}
 
 function displayComments() {
   console.log("display comments");
-  // Clear the existing comments
   parentClassUserComments.innerHTML = "";
 
   for (let i = 0; i < conversationArray.length; i++) {
     const comment = conversationArray[i];
 
-    let newImage = document.createElement("img");
+    const newImage = document.createElement("img");
     newImage.className = "user-comments-container__image-container--avatar";
 
-    let imageDiv = document.createElement("div");
+    const imageDiv = document.createElement("div");
     imageDiv.className = "user-comments-container__image-container";
     imageDiv.appendChild(newImage);
 
-    let newOuterDiv = document.createElement("div");
+    const newOuterDiv = document.createElement("div");
     newOuterDiv.className = "user-comments-container__outerdiv";
     newOuterDiv.appendChild(imageDiv);
 
-    let nameAndEmailDiv = document.createElement("div");
+    const nameAndEmailDiv = document.createElement("div");
     nameAndEmailDiv.className = "user-comments-container__name-email-div";
 
-    let newName = document.createElement("div");
+    const newName = document.createElement("div");
     newName.className = "user-comments-container__outerdiv--name";
     newName.innerHTML = comment.name;
     nameAndEmailDiv.appendChild(newName);
 
-    let newEmail = document.createElement("div");
+    const newEmail = document.createElement("div");
     newEmail.className = "user-comments-container__outerdiv--email";
-    let d = new Date(comment.timestamp);
+    const d = new Date(comment.timestamp);
     newEmail.innerHTML = d.toLocaleDateString();
     nameAndEmailDiv.appendChild(newEmail);
 
     newOuterDiv.appendChild(nameAndEmailDiv);
 
-    let newComment = document.createElement("div");
+    const newComment = document.createElement("div");
     newComment.className = "user-comments-container__outerdiv--comment";
     newComment.innerHTML = comment.comment;
     newOuterDiv.appendChild(newComment);
 
-    let likebtnContainer = document.createElement("div");
+    const likebtnContainer = document.createElement("div");
     likebtnContainer.className =
       "user-comments-container__outerdiv--likebtnContainer";
-    let likebtn = document.createElement("button");
+    const likebtn = document.createElement("button");
     likebtn.className = "user-comments-container__outerdiv--likebtn";
 
     likebtnContainer.appendChild(likebtn);
@@ -205,10 +198,10 @@ function displayComments() {
     likebtn.innerHTML = `Like ${comment.likes}`;
     newOuterDiv.appendChild(likebtnContainer);
 
-    let deletebtnContainer = document.createElement("div");
-    likebtnContainer.className =
+    const deletebtnContainer = document.createElement("div");
+    deletebtnContainer.className =
       "user-comments-container__outerdiv--deletebtnContainer";
-    let deletebtn = document.createElement("button");
+    const deletebtn = document.createElement("button");
     deletebtn.className = "user-comments-container__outerdiv--deletebtn";
 
     deletebtnContainer.appendChild(deletebtn);
@@ -216,71 +209,46 @@ function displayComments() {
     deletebtn.innerHTML = `Delete`;
     newOuterDiv.appendChild(deletebtnContainer);
 
-    let newHr = document.createElement("hr");
+    const newHr = document.createElement("hr");
     newOuterDiv.appendChild(newHr);
 
     parentClassUserComments.appendChild(newOuterDiv);
     parentClassUserComments.appendChild(newHr);
 
-    if (likebtn) {
-      likebtn.addEventListener("click", function () {
-        likebtn.innerHTML = `Like ${comment.likes + 1}`;
-        comment.likes += 1;
-        axios
-          .put(
-            `${url_index}comments/${comment.id}/like?api_key=${api_key_index}`
-          )
-          .then(function (response) {
-            console.log(response.data);
-          })
-          .catch(function (error) {
-            console.error(error);
-          });
-      });
-    } else {
-      console.log("Element with the specified selector was not found.");
-    }
+    likebtn.addEventListener("click", () => {
+      likebtn.innerHTML = `Like ${comment.likes + 1}`;
+      comment.likes += 1;
+      axios
+        .put(`${urlIndex}comments/${comment.id}/like?api_key=${apiKeyIndex}`)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
 
-    if (deletebtn) {
-      deletebtn.addEventListener("click", function () {
-        newOuterDiv.remove();
-        const hr = document.querySelector(
-          ".conversation__comment-contaier--line-break"
-        );
-        // hr.remove();
-        axios
-          .delete(
-            `${url_index}comments/${comment.id}/?api_key=${api_key_index}`
-          )
-          .then(function (response) {
-            console.log(comment.id);
-            conversationArray = conversationArray.filter(
-              (com) => com.id !== comment.id
-            );
-            displayComments();
-          })
-          .catch(function (error) {
-            console.error(error);
-          });
-      });
-    }
+    deletebtn.addEventListener("click", () => {
+      newOuterDiv.remove();
+      const hr = document.querySelector(
+        ".conversation__comment-contaier--line-break"
+      );
+
+      axios
+        .delete(`${urlIndex}comments/${comment.id}/?api_key=${apiKeyIndex}`)
+        .then((response) => {
+          console.log(comment.id);
+          conversationArray = conversationArray.filter(
+            (com) => com.id !== comment.id
+          );
+          displayComments();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
   }
 }
 
 // Call the displayComments function to display the comments initially
 displayComments();
-
-const bioLink = document.querySelector(".topnav__link--bio");
-const showsLink = document.querySelector(".topnav__link--shows");
-
-bioLink.addEventListener("click", function () {
-  bioLink.classList.add("topnav__link--active");
-  showsLink.classList.remove("topnav__link--active");
-});
-
-showsLink.addEventListener("click", function () {
-  showsLink.classList.add("topnav__link--active");
-  bioLink.classList.remove("topnav__link--active");
-});
-
-console.log(conversationArray);
